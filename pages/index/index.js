1efcc48f2564db2891143325ca505577ca46b5ef
Alex <autyan@outlook.com>
2022-07-23 16:46:03 +0800

    update bind distributorid method

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,8 @@
 // 获取应用实例
 const app = getApp()
 
+var hdService = require('../../utils/service.js');
+
 Page({
   data: {
 
@@ -9,8 +11,11 @@ Page({
   onLoad(query) {
     if(query && query.scene){
       const scene = decodeURIComponent(query.scene)
-      wx.setStorage({key:'distributor', data:scene})
-      console.log('distributorid:' + scene);
+      wx.setStorage({key:'distributor', data:scene});
+      var par = {distributorId:scene}
+      hdService.request("/api/user/binddistributor", par, "POST").then((response) => {
+        console.log("bind distributor result:" + response);
+      });
     }
   },
   toCategory(){
